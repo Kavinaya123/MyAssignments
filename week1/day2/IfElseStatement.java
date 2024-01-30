26e89feffd93f11da9be40c5055b77e8da69319f
Kavinaya123 <158147045+Kavinaya123@users.noreply.github.com>
2024-01-30 14:33:57 +0530

    Add files via upload

---
+++ week1/day2/IfElseStatement.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		        int number=50;
+		        
+		        if(number>0) {
+		            System.out.println("Positive");
+		        }
+		       
+		        
+		        else {
+		            System.out.println("Negative");
+		        }
+		        
+		       
+	}
+
+}
---
+++ week1/day2/IfElseStatemntOdd.java
@@ -0,0 +1,33 @@
+package week1.day2;
+
+public class IfElseStatemntOdd {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  //int number=51;
+	        
+	        //if(number%2==0) {
+	            //System.out.println("Even");
+	       // }
+	       
+	        
+	       // else {
+	            //System.out.println("Odd");
+	       // }
+		int a=10;
+				int b=20;
+				if((a<b)&&(a!=b)) {
+					System.out.println("True");
+				}
+				else if(a>=b) {
+					System.out.println("True");
+				}
+				else {
+					System.out.println("False");
+				}
+					
+	}
+
+}
